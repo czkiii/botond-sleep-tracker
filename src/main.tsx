3b0c4f100cd6a0c10db186d96bfa54cf461440d5
czkiii <czki.adam@gmail.com>
2026-08-23 00:28:12 +0200

    Mount Family Sync UI

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { registerSW } from 'virtual:pwa-register'
 import App from './App'
+import FamilySyncLayer from './FamilySyncLayer'
 import './styles.css'
 import './ux-tuning.css'
 import './today-fit.css'
 import './copy-overrides.css'
 import './background-theme.css'
+import './family-sync.css'
 
 registerSW({ immediate: true })
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <FamilySyncLayer />
   </React.StrictMode>
 )
