251dc3a007a8d3ee208be4a9c001cb8582c84abe
czkiii <czki.adam@gmail.com>
2026-08-22 13:34:51 +0200

    Remove conflicting spacing overrides

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import { registerSW } from 'virtual:pwa-register'
 import App from './App'
 import './styles.css'
-import './spacing-overrides.css'
 
 registerSW({ immediate: true })
 
