b09ad3beea42a6d4bb5c50a14929a6559b5bce52
czkiii <czki.adam@gmail.com>
2026-08-22 14:02:55 +0200

    Apply fixed Today layout sizing

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { registerSW } from 'virtual:pwa-register'
 import App from './App'
 import './styles.css'
 import './ux-tuning.css'
+import './today-fit.css'
 
 registerSW({ immediate: true })
 
